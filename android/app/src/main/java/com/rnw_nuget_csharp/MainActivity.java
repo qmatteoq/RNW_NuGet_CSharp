d1d9456f2e8e0be4f1b858e8836acc7ce437a643
Matteo Pagani <matteo.pagani@microsoft.com>
2022-02-15 12:07:00 +0100

    first commit

---
+++ android/app/src/main/java/com/rnw_nuget_csharp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rnw_nuget_csharp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RNW_NuGet_CSharp";
+  }
+}
